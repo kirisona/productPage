f9da003ad0256b78140c1edb1876206d1df33e62
Sofia Varich <kirisona@gmail.com>
2019-09-21 21:12:36 +0300

    markup

---
+++ js/main.js
@@ -0,0 +1,22 @@
+let order = document.querySelectorAll('.product__order');
+const popup = document.getElementById('popup');
+const popupClose = document.querySelector('.popup__close');
+const form = document.querySelector('.popup__form');
+
+for (let i = 0; i < order.length; i++) {
+  order[i].addEventListener('click', function(event) {
+    
+    popup.style.display = 'block';
+  });
+
+  form.addEventListener('click', function(event) {
+    if (!event.target && !order[i]) {
+      popup.style.display = 'none';
+    }
+  })
+}
+
+popupClose.addEventListener('click', function(event) {
+  popup.style.display = 'none';
+})
+

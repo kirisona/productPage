fc9283979bd17d13a8d844016b9df872b9103ece
Sofia Varich <kirisona@gmail.com>
2019-09-21 21:25:47 +0300

    toggle popup

--- js/main.js
+++ js/main.js
@@ -1,22 +1,33 @@
-let order = document.querySelectorAll('.product__order');
-const popup = document.getElementById('popup');
-const popupClose = document.querySelector('.popup__close');
-const form = document.querySelector('.popup__form');
+let order = document.querySelectorAll(".product__order");
+const popup = document.getElementById("popup");
+const popupClose = document.querySelector(".popup__close");
+const form = document.querySelector(".popup__form");
+
+
+
+// for (let i = 0; i < order.length; i++) {
+//   order[i].addEventListener('click', function(event) {
+
+//     popup.style.display = 'block';
+//   });
+
+//   form.addEventListener('click', function(event) {
+//     if (!event.target && !order[i]) {
+//       popup.style.display = 'none';
+//     }
+//   })
+// }
+
+// popupClose.addEventListener('click', function(event) {
+//   popup.style.display = 'none';
+// })
 
 for (let i = 0; i < order.length; i++) {
-  order[i].addEventListener('click', function(event) {
-    
-    popup.style.display = 'block';
+  order[i].addEventListener("click", function(event) {
+    popup.classList.toggle("active");
   });
-
-  form.addEventListener('click', function(event) {
-    if (!event.target && !order[i]) {
-      popup.style.display = 'none';
-    }
-  })
 }
 
 popupClose.addEventListener('click', function(event) {
-  popup.style.display = 'none';
+  popup.classList.toggle("active");
 })
-

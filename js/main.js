3ebe00ed802dc4764db2be89b9d7f8704eda1e96
Sofia Varich <kirisona@gmail.com>
2019-09-22 15:34:38 +0300

    all tasks complited, without gulp

--- js/main.js
+++ js/main.js
@@ -7,6 +7,12 @@ const bed = document.querySelector("#bed");
 const chair = document.querySelector("#chair");
 const all = document.querySelector("#all");
 let items = document.querySelectorAll(".product__item");
+let name = document.getElementById("name");
+let surname = document.getElementById("surname");
+let phone = document.getElementById("phone");
+let email = document.getElementById("email");
+let password = document.getElementById("password");
+let passwordConfirm = document.getElementById("confirm_password");
 
 for (let i = 0; i < order.length; i++) {
   order[i].addEventListener("click", function(event) {
@@ -26,33 +32,67 @@ popup.addEventListener("click", function(event) {
 });
 
 for (let i = 0; i < items.length; i++) {
-  table.addEventListener('click', function(event) {
-    if (items[i].classList.contains('table') !== true) {
-      items[i].classList.add('none');
+  table.addEventListener("click", function(event) {
+    if (items[i].classList.contains("table") !== true) {
+      items[i].classList.add("none");
     } else {
-      items[i].classList.remove('none');
+      items[i].classList.remove("none");
     }
   });
 
-  bed.addEventListener('click', function(event) {
-    if (items[i].classList.contains('bed') !== true) {
-      items[i].classList.add('none');
+  bed.addEventListener("click", function(event) {
+    if (items[i].classList.contains("bed") !== true) {
+      items[i].classList.add("none");
     } else {
-      items[i].classList.remove('none');
+      items[i].classList.remove("none");
     }
   });
 
-  chair.addEventListener('click', function(event) {
-    if (items[i].classList.contains('chair') !== true) {
-      items[i].classList.add('none');
+  chair.addEventListener("click", function(event) {
+    if (items[i].classList.contains("chair") !== true) {
+      items[i].classList.add("none");
     } else {
-      items[i].classList.remove('none');
+      items[i].classList.remove("none");
     }
   });
 
-  all.addEventListener('click', function(event) {
-    if (items[i].classList.contains('none') === true) {
-      items[i].classList.remove('none');
+  all.addEventListener("click", function(event) {
+    if (items[i].classList.contains("none") === true) {
+      items[i].classList.remove("none");
     }
-  })
+  });
+}
+
+name.oninvalid = function(event) {
+  event.target.setCustomValidity("Name should only contain letters. e.g. John");
+};
+
+surname.oninvalid = function(event) {
+  event.target.setCustomValidity(
+    "Surname should only contain letters. e.g. John"
+  );
+};
+
+phone.oninvalid = function(event) {
+  event.target.setCustomValidity(
+    "Phonenumber should only contain numbers. e.g. 380999999999"
+  );
+};
+
+email.oninvalid = function(event) {
+  event.target.setCustomValidity(
+    "Email should be similar like product@gmail.com"
+  );
+};
+
+form.addEventListener("submit", submitForm);
+
+function submitForm(event) {
+  alert(
+    "Thank you for your order. Our consultant will contact you within two hours."
+  );
+  if (password.value !== passwordConfirm.value) {
+    alert("Password and password confirmation do not match. Please try again");
+    form.removeEventListener("submit", submitForm);
+  }
 }
